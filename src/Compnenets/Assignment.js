cbbb47bc6df952ffb0553eb91c27ed4829ad4719
Bhogii <kamat2000b@gmail.com>
2023-12-04 14:45:46 +0530

    first commit

---
+++ src/Compnenets/Assignment.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Assignment() {
+  return (
+    <div>A
+        Q.1-What is Virtual DOM ?
+        Ans.Virtual DOM is an in-memory representation of the DOM. DOM refers to the Document Object Model that represents the content of HTML 
+        documents as a tree structure so that the programs can be read, accessed and changed in the document structure, style, and content.
+
+        Q.2-What is SPA?
+        Ans.SPA( Single Page Application) only need to update the currently shown page in order to respond to user, eliminating the need to repeatedly load pages from the server. 
+
+        Q.3-What is Difference between class and functional coponent?
+        Ans.A class component requires you to extend from React. Component and create a render function that returns a React element. and 
+        functional component is just a plain JavaScript pure function that accepts props as an argument and returns a React element(JSX).
+
+       Q.4-What is package.json?
+       Ans.The package.json file contains descriptive and functional metadata about a project, such as a name, version, and dependencies.
+       The file provides the npm package manager with various information to help identify the project and handle dependencies.
+
+       Q.5-What is the Difference between react and react nativ? Which one is library or framework?
+       Ans.React:-ReactJs is an open-source JavaScript library extensively used in single page app development services.
+       It is a Front-end Development Library 
+    
+       And React Native is a hybrid mobile app framework, allowing you to develop mobile applications with a single codebase. 
+       its an open-source JavaScript-based framework developed by Facebook to meet its growing demand of mobile needs.
+       It is a Native Mobile Development Library
+
+       Q.6-What is the package name you are using for routing?
+       Ans:- react-router-dom and react-router-native.
+
+
+       Q.7- What is JSX 
+       Ans.JSX(JavaScript Extension), is a React extension which allows writing JavaScript code that looks like HTML.
+
+       Q.8-What dose mean by state and its use in react?
+       Ans:-State is a way to store and manage the information or data while creating a React Application.
+       The state is s JavaScript object that contains the real-time data or information on the webpage.
+
+      Use:-React uses an observable object as the state that observes what changes are made to the state and helps the component behave accordingly.
+    </div>
+  )
+}
